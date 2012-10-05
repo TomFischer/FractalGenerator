05b6a9d382e12ed9dc1078c97905d8c00f66bed4
Thomas Fischer <thomas.fischer@ufz.de>
2012-10-05 08:16:43 +0200

    removed some old code and made some changes in file header

--- sources/GUI/Canvas.cpp
+++ sources/GUI/Canvas.cpp
@@ -14,16 +14,10 @@
 * You should have received a copy of the GNU General Public License along with
 * "FractalGenerator". If not, see <http://www.gnu.org/licenses/>.
 *
-* Author: Thomas Fischer
+* @file Canvas.cpp
 *
+*  Created on: 01/2005 by Thomas Fischer
 */
-
-/*
- * @file Canvas.cpp
- *
- *  Created on: 01/2005
- *      Author: Thomas Fischer
- */
 
 #include <cmath>
 #include <iostream>
@@ -66,12 +60,6 @@ void Canvas::on_realize()
 {
 	Gtk::DrawingArea::on_realize();
 	Glib::RefPtr<Gdk::Window> win(get_window());
-	std::cout << "[Canvas::on_realize()] size: " << get_width() << " x " << get_height() << std::endl;
-//	if (win) {
-//		Cairo::RefPtr<Cairo::Context> cairo_context (win->create_cairo_context());
-//		Gdk::Cairo::set_source_pixbuf(cairo_context, _pixbuf_image, 0, 0);
-//		on_draw(cairo_context);
-//	}
 }
 
 bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
@@ -109,7 +97,6 @@ bool Canvas::on_expose_event(GdkEventExpose* event)
 	Glib::RefPtr<Gdk::Window> window = get_window();
 	const int width (get_width());
 	const int height (get_height());
-	std::cout << "[Canvas::on_expose_event] size: " << width << " x " << height << std::endl;
 	if (window) {
 		Cairo::RefPtr<Cairo::Context> cr(window->create_cairo_context());
 
@@ -124,25 +111,6 @@ bool Canvas::on_expose_event(GdkEventExpose* event)
 
 	    // Restore context
 		cr->restore();
-
-//		const int width (getWidth());
-//		const int height (getHeight());
-//
-//		// coordinates for the center of the window
-//		int xc, yc;
-//		xc = width / 2;
-//		yc = height / 2;
-//
-//		cr->set_line_width(10.0);
-//
-//		// draw red lines out from the center of the window
-//		cr->set_source_rgb(0.8, 0.0, 0.0);
-//		cr->move_to(0, 0);
-//		cr->line_to(xc, yc);
-//		cr->line_to(0, height);
-//		cr->move_to(xc, yc);
-//		cr->line_to(width, yc);
-//		cr->stroke();
 	}
 	return true;
 }
